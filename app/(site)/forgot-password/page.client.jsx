d034d9f03cfbdbf5a9deb31bc7d0745c5fbec0f8
Code-Lucius <nemidav@gmail.com>
2026-06-26 13:01:46 +0100

    fixed reset password

--- app/(site)/forgot-password/page.client.jsx
+++ app/(site)/forgot-password/page.client.jsx
@@ -1,9 +1,23 @@
+// "use client";
+
+// import dynamic from "next/dynamic";
+
+// const ForgotPasswordComponent = dynamic(
+//   () => import("@/app-pages/forgot-password").then((mod) => mod.Component),
+//   { ssr: false }
+// );
+
+// export default function ForgotPasswordClient() {
+//   return <ForgotPasswordComponent />;
+// }
+
+
 "use client";
 
 import dynamic from "next/dynamic";
 
 const ForgotPasswordComponent = dynamic(
-  () => import("@/app-pages/forgot-password").then((mod) => mod.Component),
+  () => import("@/app-pages/forgot-password").then((mod) => mod.ForgotPassword),
   { ssr: false }
 );
 
