3fda0602ad3e6320fc15cbf1b2cee4491830f5ce
Code-Lucius <nemidav@gmail.com>
2026-07-03 12:00:02 +0100

    Restored Opay

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import success from "../assets/images/success_check.webp";
 
 export const Success = ({ children }) => {
   return (
     <div className="flex flex-col items-center mx-auto my-10 w-fit">
-      <img src={success} alt="success-verification-message" />
+      <Image
+        src={success}
+        alt="success-verification-message"
+        width={200}
+        height={200}
+      />
       {children}
     </div>
   );
