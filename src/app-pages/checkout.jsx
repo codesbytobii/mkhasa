8caa719f94d7fdfe4e453e47e4c12b71ae391621
Code-Lucius <nemidav@gmail.com>
2026-08-19 13:58:29 +0100

    made opay default provider

--- src/app-pages/checkout.jsx
+++ src/app-pages/checkout.jsx
@@ -296,7 +296,7 @@ export const Component = () => {
     },
   });
 
-  const [provider, setProvider] = useState("monnify");
+  const [provider, setProvider] = useState("opay");
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(true);
   const router = useRouter();
@@ -705,14 +705,14 @@ const PaymentMethod = ({ className, setProvider, provider }) => {
       </div>
       <div className="py-4 grid gap-4">
         <div className="flex gap-6">
-          <div className={`flex items-center gap-2 p-2 rounded-lg ${provider === "monnify" && "bg-gray-100"}`}>
+          {/* <div className={`flex items-center gap-2 p-2 rounded-lg ${provider === "monnify" && "bg-gray-100"}`}>
             <input type="radio" id="monnify" name="paymentProvider" value="monnify" checked={provider === "monnify"} onChange={handleProviderChange} />
             <label htmlFor="monnify" className="flex items-center gap-2">
               <img src="/monnify-logo.jpeg" className="w-18 h-10" alt="monnify-logo" />
             </label>
-          </div>
+          </div> */}
           <div className={`flex items-center p-2 rounded-lg gap-2 ${provider === "opay" && "bg-gray-100"}`}>
-            <input type="radio" id="opay" name="paymentProvider" value="opay" checked={provider === "opay"} onChange={handleProviderChange} />
+            <input type="radio" id="opay" name="paymentProvider" value="opay" checked={provider === "opay"} onChange={handleProviderChange}/>
             <label htmlFor="opay" className="flex items-center gap-2">
               <img src="/opay-logo.png" className="w-28 h-18" alt="opay-logo" />
             </label>
